edbfa1533c5dceb6c6a5b22cb3026021a0d4f1ed
esunhx <ascanio.mdc@gmail.com>
2023-03-17 16:45:15 -0400

    admin flow control

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -2,24 +2,69 @@ import { useState, useEffect } from 'react';
 import useEth from '../contexts/EthContext/useEth';
 import Button from '../components/UI/Button';
 import ErrorModal from '../components/UI/ErrorModal';
+import NoticeNoArtifact from '../components/Notice/NoticeNoArtifact';
+import NoticeWrongNetwork from '../components/Notice/NoticeWrongNetwork';
 
 const Admin = () => {
   const {
-    state: { contract, web3, accounts, status }
+    state: { artifact, contract, accounts, status }
   } = useEth();
   const [voter, setVoter] = useState('');
   const [error, setError] = useState();
+  const [owner, setOwner] = useState(false);
+  const [votersRegistered, setVotersRegistered] = useState(false)
+
+  useEffect(() => {
+    const checkOwner = async () => {
+      if (contract) {
+        const ownerAddr = await contract.methods.owner().call();
+        if (accounts[0]===ownerAddr) {
+          setOwner(true);
+        } else {
+          setOwner(false);
+        }
+      }
+    }
+    checkOwner();
+  }, [contract, accounts]);
 
   const addVoterHandler = async () => {
-    await contract.methods.addVoter(voter).send({ from: accounts[0] });
+    if (!owner) {
+      setError({
+        title: 'Not Admin',
+        message: 'You are not authorised'
+      })
+    } else {
+      await contract.methods.addVoter(voter).send({ from: accounts[0] });
+    }
   };
 
   const voterHandler = (event) => {
-    setVoter(event.target.value);
+    if (!owner) {
+      setError({
+        title: 'Not Admin',
+        message: 'You are not authorised'
+      })
+    } else {
+      setVoter(event.target.value);
+    }
   };
 
   const startRegistrationHandler = async () => {
-    await contract.methods.startProposalsRegistering().send({ from: accounts[0] });
+    if (status !== 'ResiteringVoters') {
+      setError({
+        title: 'Bad Status',
+        message: 'You cannot do that now'
+      });
+    } else if (!owner) {
+      setError({
+        title: 'Not Admin',
+        message: 'You are not authorised'
+      })
+    } else {
+      await contract.methods.startProposalsRegistering().send({ from: accounts[0] });
+      setVotersRegistered(true);
+    }
   };
 
   const endRegistrationHandler = async () => {
@@ -28,51 +73,117 @@ const Admin = () => {
         title: 'Bad Status',
         message: 'You cannot do that now'
       });
+    } else if (!owner) {
+      setError({
+        title: 'Not Admin',
+        message: 'You are not authorised'
+      })
     } else {
       await contract.methods.endProposalsRegistering().send({ from: accounts[0] });
     }
   };
 
   const startVotingHandler = async () => {
-    if (status !== 'ProposalsRegistrationEnd') {
+    if (status !== 'ProposalsRegistrationEnded') {
       setError({
         title: 'Bad Status',
         message: 'You cannot do that now'
       });
+    } else if (!owner) {
+      setError({
+        title: 'Not Admin',
+        message: 'You are not authorised'
+      })
     } else {
       await contract.methods.startVotingSession().send({ from: accounts[0] });
     }
   };
 
   const endVotingHandler = async () => {
-    await contract.methods.endVotingSession().send({ from: accounts[0] });
+    if (status !== 'VotingSessionStarted') {
+      setError({
+        title: 'Bad Status',
+        message: 'You cannot do that now'
+      });
+    } else if (!owner) {
+      setError({
+        title: 'Not Admin',
+        message: 'You are not authorised'
+      })
+    } else {
+      await contract.methods.endVotingSession().send({ from: accounts[0] });
+    }
   };
 
   const tallyVotesHandler = async () => {
-    await contract.methods.tallyVotes().send({ from: accounts[0] });
+    if (status !== 'VotingSessionEnded') {
+      setError({
+        title: 'Bad Status',
+        message: 'You cannot do that now'
+      });
+    } else if (!owner) {
+      setError({
+        title: 'Not Admin',
+        message: 'You are not authorised'
+      })
+    } else {
+      await contract.methods.tallyVotes().send({ from: accounts[0] });
+    }
   };
 
   const errorHandler = () => {
     setError(null);
   };
 
+  const admin = (
+    <>
+    {error && <ErrorModal title={error.title} message={error.message} onClick={errorHandler} />}
+        <div className="mt-6 mx-auto w-2/3">
+          {/* <div className="flex flex-row justify-between text-center"> */}
+
+          <div className="flex flex-row justify-between text-center">
+            <div className="flex flex-col lg:flex-row items-center lg:space-x-4 text-slate-700 align">
+              <label>Workflow Status:</label>
+              <span className="text-sky-800 text-xl border border-sky-800 rounded-lg p-2 lg:mt-0">
+                {status}
+              </span>
+            </div>
+          </div>
+          { status==="RegisteringVoters" &&(
+            <>
+            <input className="rounded-md h-8 p-2 my-2 border border-sky-800" onChange={voterHandler} />
+            <Button label="Add Voter" onClick={addVoterHandler} /><div className="space-x-4">
+            <Button label="Start Proposal" onClick={startRegistrationHandler} />
+            </div>
+            </>
+          )}
+          {status==="ProposalsRegistrationStarted" && (
+            <div className="space-x-4">
+              <Button label="End Proposal" onClick={endRegistrationHandler} />
+            </div>
+          )}
+          {status==="ProposalsRegistrationEnded" && (
+            <div className="space-x-4">
+              <Button label="Start Voting" onClick={startVotingHandler} />
+            </div>
+          )}
+          {status==="VotingSessionStarted" && (
+            <div className="space-x-4">
+              <Button label="End Voting" onClick={endVotingHandler} />
+            </div>
+          )}
+          {status==="VotingSessionEnded" && (
+            <div className="space-x-4">
+              <Button label="Tally Vote" onClick={tallyVotesHandler} />
+            </div>
+          )}
+        </div>    
+    </>
+  )
+
   return (
-    <div>
-      {error && <ErrorModal title={error.title} message={error.message} onClick={errorHandler} />}
-      <h1>Hello Admin!</h1>
-      <input className="border-black border" onChange={voterHandler} />
-      <Button label="Add Voter" onClick={addVoterHandler} />
-      <Button label="Start Proposal" onClick={startRegistrationHandler} />
-      <Button label="End Proposal" onClick={endRegistrationHandler} />
-      <Button label="Start Voting" onClick={startVotingHandler} />
-      <Button label="End Voting" onClick={endVotingHandler} />
-      <Button label="Tally Vote" onClick={tallyVotesHandler} />
-      <div>
-        Workflow Status:{' '}
-        <span className="text-slate-700 text-xl border border-slate-700 rounded-lg p-2 my-2">
-          {status}
-        </span>
-      </div>
+    <div className="flex flex-col mx-auto bg-background bg-cover min-h-screen">
+      {!artifact ? <NoticeNoArtifact /> : !contract ? <NoticeWrongNetwork /> : admin}
     </div>
   );
 };
